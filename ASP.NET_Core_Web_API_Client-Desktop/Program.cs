5543123268519803d78dc1b529b672b0db093245
pjslot <pjslot@gmail.com>
2023-01-22 22:48:44 +0300

    release 1.0

--- ASP.NET_Core_Web_API_Client-Desktop/Program.cs
+++ ASP.NET_Core_Web_API_Client-Desktop/Program.cs
@@ -1,4 +1,5 @@
-﻿//добавать через нугет newtonsoft json и microsoft.aspnet.webapi.client
+﻿//Kabluchkov DS (c) 2023
+//добавать через нугет newtonsoft json и microsoft.aspnet.webapi.client
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -12,80 +13,136 @@ namespace ASP.NET_Core_Web_API_Client_Desktop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Вывод всей базы!");
-            GetAllProducts();
+            //МЕНЮ
+            bool exit = false;
+            do
+            {                
+                Console.Clear();
+                Console.WriteLine("Главное меню: \n 1) Вывод всей базы. \n 2) Удаление продукта по ID. \n 3) Изменение продукта по ID. \n 4) Вывод всех продуктов по определённой цене." +
+                    " \n 5) Вывод количества продуктов по определённому имени. \n 6) Вывод средней цены по товарам. \n 7) Добавление скидки на все товары. \n 8) Добавление скидки на определённое наименование. \n 9) Добавление продукта. \n 10) Выход.");
+                string menuNum = Console.ReadLine();
+                switch (menuNum)
+                {
 
-            Console.WriteLine("Жмём кнопку!");
-            Console.ReadKey();
-            Console.WriteLine();
+                    //Вывод всей базы
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Вывод всей базы!");
+                        GetAllProducts();
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            Console.WriteLine("Добавляем сыр!");
-            AddProduct("cheese", 400);
-            Console.WriteLine("Жмём кнопку!");
-            Console.ReadKey();
-            Console.WriteLine();
+                    //Удаление продукта по ID
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Удаление продукта по ID.");
+                        Console.WriteLine("Введите ID продукта для удаления:");
+                        string id = Console.ReadLine();
+                        DeleteProduct(id);                       
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            //Console.WriteLine("Вывод всей базы!");
-            //GetAllProducts();
-            //Console.WriteLine("Жмём кнопку!");
-            //Console.ReadKey();
+                    //Изменение продукта по ID
+                    case "3":
+                        Console.WriteLine("Изменение продукта по ID.");
+                        Console.WriteLine("Введите ID продукта для изменения:");
+                        id = Console.ReadLine();
+                        Console.WriteLine("Введите новое название продукта:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Введите новую цену продукта:");
+                        int price = Convert.ToInt32(Console.ReadLine());
 
+                        Product productToPut = new Product
+                        {
+                            ProductID = id,
+                            Name = name,
+                            Price = price
+                        };
 
-            //Console.WriteLine("удаляем томат!");
-            //DeleteProduct("7faecc5e-245b-49a4-9082-e090edc5765a");
-            //Console.WriteLine("Жмём кнопку!");
-            //Console.ReadKey();
-            //Console.WriteLine();
+                        PutProduct(productToPut);
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            //Console.WriteLine("Вывод всей базы!");
-            //GetAllProducts();
-            //Console.WriteLine("Жмём кнопку!");
-            //Console.ReadKey();
+                    //Вывод всех продуктов по определённой цене
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Вывод товаров по определённой цене.");
+                        Console.WriteLine("Введите цену:");
+                        price = Convert.ToInt32(Console.ReadLine());
+                        GetProductsByPrice(price);
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            //Console.WriteLine("меняем один из сыров");
-            //Product productToPut = new Product
-            //{
-            //    ProductID = "f3fd619a-2a92-482a-b4f5-f0e9a2478aec",
-            //    Name = "Cheeeese",
-            //    Price = 100000
-            //};
+                    //Вывод количества продуктов по определённому имени
+                    case "5":
+                        Console.Clear();
+                        Console.WriteLine("Вывод количества продуктов по определённому имени.");
+                        Console.WriteLine("Введите имя продукта:");
+                        name = Console.ReadLine();
+                        GetNumberOfProductsByName(name);
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            //PutProduct(productToPut);
-            //Console.WriteLine("Жмём кнопку!");
-            //Console.ReadKey();
-            //Console.WriteLine();
+                    //Вывод средней цены по товарам
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("Вывод средней цены по товарам.");
+                        GetPriceAverage();
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            //Console.WriteLine("Вывод по ценнику 400");
-            //GetProductsByPrice(400);
-            //Console.WriteLine("Жмём кнопку!");
-            //Console.ReadKey();
+                    //Добавление скидки на все товары
+                    case "7":
+                        Console.Clear();
+                        Console.WriteLine("Добавление скидки на все товары.");
+                        Console.WriteLine("Введите размер скидки в процентах:");
+                        int discount = Convert.ToInt32(Console.ReadLine());
+                        PutTotalDiscount(discount);
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            //Console.WriteLine("Вывод количества сыров");
-            //GetNumberOfProductsByName("cheese");
-            //Console.WriteLine("Жмём кнопку!");
-            //Console.ReadKey();
+                    //Добавление скидки на определённое наименование
+                    case "8":
+                        Console.Clear();
+                        Console.WriteLine("Добавление скидки на определённое наименование.");
+                        Console.WriteLine("Введите имя продукта:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Введите размер скидки в процентах:");
+                        discount = Convert.ToInt32(Console.ReadLine());
+                        PutNameDiscount(name, discount);
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            //Console.WriteLine("Вывод средней цены по товарам");
-            //GetPriceAverage();
-            //Console.WriteLine("Жмём кнопку!");
-            //Console.ReadKey();
+                    //Добавление продукта
+                    case "9":
+                        Console.Clear();
+                        Console.WriteLine("Добавление продуата в базу.");
+                        Console.WriteLine("Введите имя продукта:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Введите стоимость продукта:");
+                        price = Convert.ToInt32(Console.ReadLine());
+                        AddProduct(name, price);
+                        Console.WriteLine("Конец команды! Press Any Key.");
+                        Console.ReadKey();
+                        break;
 
-            //Console.WriteLine("Добавление скидки на всё 50%");
-            //PutTotalDiscount(50);
-            //Console.WriteLine("Жмём кнопку!");
-            //Console.ReadKey();
+                    //выход
+                    case "10":
+                        exit= true;
+                        break;
 
-            Console.WriteLine("Добавление скидки на сыр 50%");
-            PutNameDiscount("cheese", 50);
-            Console.WriteLine("Жмём кнопку!");
-            Console.ReadKey();
+                }
 
-            Console.WriteLine("Вывод всей базы!");
-            GetAllProducts();
-            Console.ReadKey();
-
-
-
+            } while (exit != true);
+            
         }
 
         //метод вывода всей базы
@@ -97,8 +154,6 @@ namespace ASP.NET_Core_Web_API_Client_Desktop
             HttpResponseMessage response = await client.GetAsync(baseUrl);
             if (response.IsSuccessStatusCode)
             {
-                //  string jsonresp = await response.Content.ReadAsStringAsync();
-                //   Console.WriteLine(jsonresp);
                 List<Product> products = await response.Content.ReadAsAsync<List<Product>>();
                 foreach (var p in products)
                 {
